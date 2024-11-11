c4788bade54261a3708fb6fc10e38aae785d9d03
victor-ok <vua.okafor@gmail.com>
2024-11-11 23:34:31 +0100

    basic authimplementation

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+""" Authentication module
+"""
+
+from flask import request
+
+
+class Auth:
+    """
+    Auth class
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        To be inmplemented
+        """
+        if not path or not excluded_paths:
+            retrun True
+        
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        retrives and receives request headers
+        Arg:
+            request Flask request obj
+        """
+        if request:
+            header = header.headers.get("Authorization")
+            return header
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        """
+        return None
+
